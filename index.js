8b2aba3480b5aa4a143315b4731d4d6ffc556b01
Mark Sarcone <mdsarcone@gmail.com>
2017-08-31 20:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name) {
+  
+}
+
+function happyHolidayTo(holiday, name) {
+  
+}
+
+function holidayCountdown(holiday, days) {
+  
+}
